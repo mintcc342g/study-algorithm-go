bab7bf9ac231843516564ecee170f5036259f86c
mintcc342g <>
2025-03-10 18:24:58 +0900

    add problems

--- revnote/top150.go
+++ revnote/top150.go
@@ -269,3 +269,182 @@ func sbfs(x, y int, board [][]byte) {
 	sbfs(x, y+1, board)
 	sbfs(x, y-1, board)
 }
+
+/*
+ **
+ * 215. Kth Largest Element in an Array
+ * k번째로 큰 수 찾기
+ * 효율이 가장 좋은 heap sort를 사용해서 해결
+ * 효율이 나쁘지만 가능한 방법은 quick selection 정도인듯?
+ */
+func findKthLargest(nums []int, k int) int {
+	// k번째로 큰 수 찾기
+	// 배열이 들어오므로, heap sort로 우선순위큐로 만들면서 정렬
+	// 그리고 k번째까지 root를 마지막 leaf로 바꾸면서 재구성하면 됨.
+
+	l := len(nums) - 1
+	for i := l / 2; i > 0; i-- { // 배열의 절반뒤는 항상 리프노드니까 앞 절반을 root로 넣어서 heap 구성
+		heapify(nums, i, 0, l)
+	}
+
+	ret := 0
+	for k > 0 {
+		ret = nums[0]
+		nums[0], nums[l] = nums[l], nums[0]
+		l--
+		heapify(nums, 0, 0, l)
+		k--
+	}
+
+	return ret
+}
+
+func heapify(nums []int, parent, min, max int) {
+	left := parent*2 + 1  // 공식
+	right := parent*2 + 2 // 공식
+	largest := parent
+
+	if left >= min && left <= max && nums[left] > nums[largest] {
+		largest = left
+	}
+	if right >= min && right <= max && nums[right] > nums[largest] {
+		largest = right
+	}
+
+	if parent != largest {
+		nums[parent], nums[largest] = nums[largest], nums[parent]
+		heapify(nums, largest, min, max)
+	}
+}
+
+/*
+ **
+ * 373. Find K Pairs with Smallest Sums
+ * 주어진 배열 2개에서 숫자 하나씩 뽑아서 만든 합이 최소인 k개의 쌍을 리턴하는 문제
+ * 배열은 오름차순으로 들어옴.
+ */
+func kSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
+	// k번째 작은 수 등은 heap sort 가 유리한데, 여기서는 두 배열의 합을 만들어야 함.
+	// 리턴해야 하는 건 두 배열의 각 인덱스이므로, 인덱스도 갖고 있어야 됨.
+	// 따라서 두 배열의 합을 노드값으로 삼는 min heap 만들어야 함.
+
+	// 단, 이러면 두 배열의 모든 조합을 만들어야 하므로 효율적이지 않음.
+	// 따라서, nums1의 모든 수와 nums2의 0번째 값만으로 k개까지만 힙을 만들어줌.
+	// (오름차순으로 오니까 어쨌든 둘 중 한 배열의 0번째 값만 쓰는 게 값이 작을 거라서.)
+
+	// 그리고 k번째까지 쭉 돌면서 최소값 인덱스 뽑아내고, 값 하나씩 뽑아낼 때마다 힙을 재정렬함.
+	// 이 때 어차피 힙을 재정렬해야 하므로, nums2의 수들을 뽑으면서 노드에 넣으면서 재정렬 해줌.
+
+	// 구현 방법
+	// 1. min heap 자료구조를 구현함. (upheap, downheap 구현 필요)
+	// 2. min heap으로 k개까지 힙을 만들어줌.
+	// 3. k번째까지 거꾸로 돌면서 1) 힙에서 노드를 하나씩 뽑고, 2) nums2[*] 로 힙을 다시 구성해줌.
+	//    - 거꾸로 도는 이유는 변수 더 선언하기 싫어서인듯. 0부터 시작하려면 또다른 변수 선언해줘야 하니까
+	heap := NewMinHeap()
+	for i := 0; i < len(nums1) && i < k; i++ {
+		heap.Push(Pair{sum: nums1[i] + nums2[0], i: i, j: 0})
+	}
+
+	ret := make([][]int, 0)
+	for k > 0 {
+		p := heap.Pop()
+		ret = append(ret, []int{nums1[p.i], nums2[p.j]})
+
+		if p.j+1 < len(nums2) {
+			heap.Push(Pair{sum: nums1[p.i] + nums2[p.j+1], i: p.i, j: p.j + 1})
+		}
+
+		k--
+	}
+
+	return ret
+}
+
+type Pair struct {
+	sum int
+	i   int
+	j   int
+}
+
+type MinHeap struct {
+	pairs []Pair
+}
+
+func NewMinHeap() *MinHeap {
+	return &MinHeap{pairs: []Pair{}}
+}
+
+// 받은 값을 힙의 가장 마지막 위치에 넣고 root로 올라오면서 자리 찾아줌.
+func (r *MinHeap) Push(p Pair) {
+	r.pairs = append(r.pairs, p)
+	r.upHeap(len(r.pairs) - 1)
+}
+
+func (r *MinHeap) upHeap(child int) {
+	parent := (child - 1) / 2 // 부모 찾는 공식
+	if parent >= 0 && r.pairs[parent].sum > r.pairs[child].sum {
+		r.pairs[parent], r.pairs[child] = r.pairs[child], r.pairs[parent]
+		r.upHeap(parent)
+	}
+}
+
+// 데이터 삭제 시에 사용
+// 루트를 복제하고, 마지막 자식 노드를 루트로 올리고, 마지막 자식 노드를 삭제한 후에,
+// 루트에 있는 값이 자리를 root -> leaf 순으로 찾아가게 해줌.
+func (r *MinHeap) Pop() Pair {
+	last := len(r.pairs) - 1
+	if last < 0 {
+		panic("empty heap")
+	}
+
+	root := r.pairs[0]
+	r.pairs[0], r.pairs[last] = r.pairs[last], r.pairs[0]
+	r.pairs = r.pairs[:last]
+	r.downHeap(0)
+
+	return root
+}
+
+// 입력받은 parent 위치부터 root -> leaf 순으로 내려가면서 정렬을 해줌.
+func (r *MinHeap) downHeap(parent int) {
+	left := parent*2 + 1  // 0-based 공식
+	right := parent*2 + 2 // 0-based 공식
+	smallest := parent
+
+	if 0 <= left && left < len(r.pairs) && r.pairs[parent].sum > r.pairs[left].sum {
+		smallest = left
+	}
+	if 0 <= right && right < len(r.pairs) && r.pairs[parent].sum > r.pairs[right].sum {
+		smallest = right
+	}
+	if smallest != parent {
+		r.pairs[smallest], r.pairs[parent] = r.pairs[parent], r.pairs[smallest]
+		r.downHeap(smallest)
+	}
+}
+
+/*
+ **
+ * 136. Single Number
+ * 배열에서 2번 안 나온 수 찾아서 리턴하기
+ * 단, linear 시간 복잡도, 상수 공간 복잡도 안에 해결해야 함.
+ * 즉 map 쓰면 안 되고, 다른 방법을 찾아야 함.
+ */
+func singleNumber(nums []int) int {
+	// 비트 연산 중 ^ 를 써야함.
+	// 1개 수에 ^ 이거 쓰면(단항 연산자), 1은 0으로 0은 1로 바꿈. (NOT)
+	// 근데 2개 수를 ^ 로 계산할 경우 둘을 합치는데(이항 연산자),
+	// 수가 같으면 0이고, 수가 다르면 1로 바꿈. (XOR)
+	// 예를 들어, 0100 + 1000 이라면 1100 이 된다는 뜻
+	// 이걸 int로 보자면, 1 ^ 1 값은 0이 됨.
+	// 근데 0 ^ 3 이라면 값은 3이 됨.
+	// 따라서, nums 중 1개 수만 2번이 안 나오므로,
+	// 걍 for문 돌리면서 이항 연산을 해주면 됨.
+
+	res := 0
+	for _, n := range nums {
+		res ^= n
+	}
+
+	return res
+}
